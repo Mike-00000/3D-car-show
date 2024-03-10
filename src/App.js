bac3ca86a23b73de504a422dd7f4ecbc1813b421
Mike <mt.mail.mt@gmail.com>
2024-03-10 12:54:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+
+function CarShow() {
+  return null;
+}
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
   );
 }
 
